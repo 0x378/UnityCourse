5ffeedaad81fa6a53fcffc2a2556c623a23ea4b3
The Cat <57572257+0x378@users.noreply.github.com>
2021-04-13 19:45:59 -0700

    Hero Alternate

--- 02_Hero/Hero/Assets/Hero.cs
+++ 02_Hero/Hero/Assets/Hero.cs
@@ -10,8 +10,9 @@ public class Hero : MonoBehaviour
 
     // Initialized upon startup:
     private int health;
-    private bool mouseMode;          // 0 = mouse, 1 = keyboard
-    private bool damageEnabled;      // 0 = no player damage, 1 = enabled
+    private bool mouseMode;         // 0 = keyboard only, 1 = mouse
+    private bool damageEnabled;     // 0 = no player damage, 1 = enabled
+    private float velocity;         // Initial value: 20 units/sec (for keyboard-only mode)
 
     private int imageState;         // Array index for sprite images
     private int numberOfImages;     // Number of sprite images in the array
@@ -19,8 +20,7 @@ public class Hero : MonoBehaviour
     private float currentImageTime; // The time at which the current sprite image appeared
     private SpriteRenderer currentSprite;
 
-    private Vector3 acceleration;
-    private Vector3 velocity;
+    // Reinitialized upon each update:
     private Vector3 position;
     private Vector3 scene;
     private Vector3 mouse;
@@ -33,9 +33,11 @@ public class Hero : MonoBehaviour
         damageEnabled = false;
         imageState = 0;
         numberOfImages = sprites.Length;
-        maximumImageTime = 60 / (3 * numberOfImages * propellerRPM);
+        maximumImageTime = 60f / (3f * numberOfImages * propellerRPM);
         currentImageTime = Time.time;
         currentSprite = gameObject.GetComponent<SpriteRenderer>();
+
+        velocity = 20f;
     }
 
     private void UpdateSpriteImage()
